c1371d0eb1c759c717fded58e7f02a571979eba5
Denis Yakorev <denisyakorev@mail.ru>
2018-08-06 21:24:13 +0300

    make tree

--- src/components/PubTreeJQ.js
+++ src/components/PubTreeJQ.js
@@ -1,22 +1,25 @@
 import 'rc-tree/assets/index.css';
 import React from 'react';
 import $ from 'jquery'
-import 'vakata-jstree/dist/jstree.js'
+import 'jstree/dist/jstree'
+
 
 
 
 class PubTreeJQ extends React.Component{
 
     componentDidMount(){
-        $('#tree-holder').jstree(this.props.pub_data.pub_data.content_json);
-        console.log('finish');
+        var obj = JSON.parse(this.props.pub_data.pub_data.content_json);
+        $(function() {
+          $('#tree-holder').jstree(obj);
+        });
+
     }
 
 
     render() {
         return(
             <div>
-
                 <div className='fdb-box fdb-touch'>
                     <h2>{this.props.pub_data.pub_data.name}</h2>
                     <div id='tree-holder'></div>
