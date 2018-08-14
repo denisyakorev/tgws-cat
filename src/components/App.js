462f74135bee092eea5e97e2ab92d45ffe58e8bd
Denis Yakorev <denisyakorev@mail.ru>
2018-08-14 00:35:38 -0700

    change tree width

--- src/components/App.js
+++ src/components/App.js
@@ -20,9 +20,18 @@ class App extends Component{
 				<div className='fdb-block'>
 					<div className="container">
 						<div className="row">
-							<div className="col-12 col-md-8 m-auto col-lg-4">
+							<div className="col-12 col-md-8 m-auto col-lg-10">
 								<PubTreeJQ />
 							</div>
+							<div className="col-sm-8 col-lg-2">
+								
+							</div>								
+						</div>
+					</div>
+				</div>
+				<div className='fdb-block'>
+					<div className="container">
+						<div className="row">							
 							<div className="col-sm-8">
 								<Content />
 							</div>
